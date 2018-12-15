bb19da724dd34074a38085012e9b195086d38a06
Paweł <pawelokrasa198@gmail.com>
2018-12-15 19:08:14 +0100

    Added unit testing project

---
+++ BookingAppxUnitTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace BookingAppxUnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+       
+    }
+}
