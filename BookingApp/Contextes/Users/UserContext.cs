b86937b1073a78dc9d9248510044d8473563bd00
Paweł <pawelokrasa198@gmail.com>
2018-12-17 22:53:24 +0100

    Creating logic for users register/loggin/authentication TO DO: -refactore all code for user authentication logic -do rest of work for authentication -connect to azure db -ensure connection strings

---
+++ BookingApp/Contextes/Users/UserContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BookingApp.Entities.Users;
+
+namespace BookingApp.Contextes.Users
+{
+    public class UserContext : DbContext
+    {
+        public UserContext(DbContextOptions<UserContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ BookingApp/Helpers/AutoMapperProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using BookingApp.Dtos.User;
+using BookingApp.Entities.User;
+
+namespace BookingApp.Helpers
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<User, UserDto>();
+            CreateMap<UserDto, User>();
+        }
+    }
+}
