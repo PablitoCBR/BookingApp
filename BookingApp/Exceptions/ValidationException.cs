6c345897111e274faaac8ba4e50e3fbe33dd4166
Paweł <pawelokrasa198@gmail.com>
2019-02-19 21:50:21 +0100

    Created new exceptions types

--- BookingApp/Exceptions/ValidationException.cs
+++ BookingApp/Exceptions/ValidationException.cs
@@ -5,7 +5,7 @@ namespace BookingApp.Exceptions
 {
     public class ValidationException : Exception
     {
-        public object Arguments { get; set; }
+        public object[] Arguments { get; set; }
         public ValidationException(string message, object[] arguments) : base(message)
         {
             Arguments = arguments;
