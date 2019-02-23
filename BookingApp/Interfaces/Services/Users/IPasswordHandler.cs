db1947ef09956b3dc149f959262f2f3cef0cb1bf
Paweł <pawelokrasa198@gmail.com>
2019-02-23 12:36:33 +0100

    Removing, renaming old files

--- BookingApp/Interfaces/Services/Users/IPasswordHandler.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BookingApp.Interfaces.Services.Users
-{
-    public interface IPasswordHandler
-    {
-        void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt);
-        bool VerifyPasswordHash(string password, byte[] storedHash, byte[] storedSalt);
-    }
-}
