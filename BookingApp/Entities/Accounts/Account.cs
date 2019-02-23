5a00ca92755e23f644802913f7351e3063976d35
Paweł <pawelokrasa198@gmail.com>
2019-02-23 15:00:11 +0100

    New accounts data models

---
+++ BookingApp/Entities/Accounts/Account.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookingApp.Entities.Accounts
+{
+    public class Account
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public byte[] PasswordHash { get; set; }
+        [Required]
+        public byte[] PasswordSalt { get; set; }
+    }
+}
---
+++ BookingApp/Entities/Accounts/Business.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BookingApp.Entities.Accounts
+{
+    public class Business : Account
+    {
+        [Required]
+        public string CompanyName { get; set; }
+        [Required]
+        [DataType(DataType.PhoneNumber)]
+        public string PhoneNumber { get; set; }
+        [Required]
+        public virtual Address Address {get; set;}
+        [Required]
+        [ForeignKey("Address")]
+        public int AddressId { get; set; }
+    }
+}
