565e044995f87ef3f4de84cb20dc55a28d8b7267
Paweł <pawelokrasa198@gmail.com>
2019-03-15 12:24:09 +0100

    Schedule controller

--- BookingApp/Controllers/ScheduleController.cs
+++ BookingApp/Controllers/ScheduleController.cs
@@ -1,15 +1,89 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
+﻿using BookingApp.Interfaces.Services.Schedules;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using BookingApp.Entities.Accounts;
+using BookingApp.Dtos.Schedules;
+using System;
+using BookingApp.Exceptions;
 
 namespace BookingApp.Controllers
 {
+    [Authorize(Roles = Role.Business)]
     [Route("api/[controller]")]
     [ApiController]
     public class ScheduleController : ControllerBase
     {
+        private readonly IScheduleService _scheduleService;
+
+        public ScheduleController(IScheduleService scheduleService)
+        {
+            _scheduleService = scheduleService;
+        }
+
+        [AllowAnonymous]
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            try
+            {
+                ScheduleDto schedule = _scheduleService.Get(id);
+                return Ok(schedule);
+            }
+            catch(ValidationException ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
+        }
+
+        [HttpPost("{id}")]
+        public IActionResult Get(int id, [FromBody]ScheduleDto scheduleDto)
+        {
+            if (Convert.ToInt32(User.Identity.Name) != id)
+                return Unauthorized();
+
+            try
+            {
+                _scheduleService.Create(id, scheduleDto);
+                return Ok();
+            }
+            catch(ValidationException ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, [FromBody]ScheduleDto scheduleDto)
+        {
+            if (Convert.ToInt32(User.Identity.Name) != id)
+                return Unauthorized();
+
+            try
+            {
+                _scheduleService.Update(id, scheduleDto);
+                return Ok();
+            }
+            catch(ValidationException ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Remove(int id)
+        {
+            if (Convert.ToInt32(User.Identity.Name) != id)
+                return Unauthorized();
+
+            try
+            {
+                _scheduleService.Remove(id);
+                return Ok();
+            }
+            catch (ValidationException ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
+        }
     }
 }
